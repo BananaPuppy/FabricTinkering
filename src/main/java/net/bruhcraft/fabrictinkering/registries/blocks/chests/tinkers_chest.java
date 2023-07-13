ba67918bc7a4cdc71731eebe6b95c68c151bb978
BananaPuppy <bananabaconpancakes07@gmail.com>
2023-07-12 20:24:55 -0700

    Working through Materials and You: Updated Chest's VoxelShape

--- src/main/java/net/bruhcraft/fabrictinkering/registries/blocks/chests/tinkers_chest.java
+++ src/main/java/net/bruhcraft/fabrictinkering/registries/blocks/chests/tinkers_chest.java
@@ -31,11 +31,12 @@ public class tinkers_chest extends HorizontalFacingBlock {
     or  MainClassClient BlockRenderLayerMap.INSTANCE.putBlock(ExampleMod.MY_BLOCK, RenderLayer.getTranslucent());
      */
     private static final VoxelShape SHAPE = Stream.of(
-            Block.createCuboidShape(12, 0, 12, 16, 12, 16),
-            Block.createCuboidShape(0, 0, 12, 4, 12, 16),
-            Block.createCuboidShape(12, 0, 0, 16, 12, 4),
-            Block.createCuboidShape(0, 0, 0, 4, 12, 4),
-            Block.createCuboidShape(0, 12, 0, 16, 16, 16)
+            Block.createCuboidShape(0, 15, 0, 16, 16, 16),
+            Block.createCuboidShape(1, 3, 1, 15, 15, 15),
+            Block.createCuboidShape(0.5, 0, 0.5, 2.5, 15, 2.5),
+            Block.createCuboidShape(0.5, 0, 13.5, 2.5, 15, 15.5),
+            Block.createCuboidShape(13.5, 0, 13.5, 15.5, 15, 15.5),
+            Block.createCuboidShape(13.5, 0, 0.5, 15.5, 15, 2.5)
     ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
 
     @SuppressWarnings("deprecation")
